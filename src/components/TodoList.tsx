e6b8bd4a824856a97fe58e154315f910c2f18f66
jhchoi1182 <jhchoi1182@gmail.com>
2023-03-22 23:40:18 +0900

    카테고리 별 todo 분리 작업 (feat. 배열 구조 분해 할당)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,38 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
-import { todoState } from "../atoms";
+import { todoSelector, todoState } from "../atoms";
 import CreateTodo from "./CreateTodo";
 import Todo from "./Todo";
 
 const TodoList = () => {
-  const todos = useRecoilValue(todoState);
+  const [todo, doing, done] = useRecoilValue(todoSelector);
+  console.log(todo);
+
   return (
     <>
       <h1>투두 리스트</h1>
       <CreateTodo />
+      <h2>To Do</h2>
       <ul>
-        {todos.map((todo) => (
+        {todo.map((todo) => (
           <Todo key={todo.id} {...todo} />
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((todo) => (
+          <Todo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((todo) => (
+          <Todo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <hr />
     </>
   );
 };
